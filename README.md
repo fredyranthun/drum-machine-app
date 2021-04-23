309ef00948249c10fc9b7bf461789f16e4dcfe87
fredyranthun <fredyranthun@gmail.com>
2021-04-23 19:47:32 -0300

    First commit for uploading and deplotment.

--- README.md
+++ README.md
@@ -1,43 +1,22 @@
-# Getting Started with Create React App
+# Drum Machine App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Used resources:
+- Use Sound Hook - https://github.com/joshwcomeau/use-sound
+- Use event listener - https://github.com/donavon/use-event-listener
+- Styled Components;
+- React-range-slider - https://github.com/whoisandy/react-rangeslider#readme
+- React-switch - https://github.com/markusenglund/react-switch#readme
+- sounds from https://freesound.org/
 
-## Available Scripts
+## App features;
 
-In the project directory, you can run:
+The app allows you to change the volume of the sounds, input by clicking the buttons and by pressing the indicated keys.
+Besides, you can turn it off on the Switch element.
 
-### `yarn start`
+This app was based on a Free Code Camp (https://www.freecodecamp.org/) challenge with some adaptations - https://www.freecodecamp.org/learn/front-end-libraries/front-end-libraries-projects/build-a-drum-machine.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+In this challenge I opted by using just React and SASS (of the mentioned libraries) and resources above, not other libraries (as Redux, Bootstrap and jQuery).
 
 ## Learn More
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+// import 'react-rangeslider/lib/index.css';
+import './App.scss';
+import React, { useState } from 'react';
+import useSound from 'use-sound';
+import drumKick from './sounds/sound-files/drum-kick.mp3';
+import snare from './sounds/sound-files/snare.wav';
+import cowbell from './sounds/sound-files/cowbell.mp3';
+import cowbell2 from './sounds/sound-files/baby-cowbell.mp3';
+import chord from './sounds/sound-files/cminor7.mp3';
+import ride from './sounds/sound-files/ride.mp3';
+import hithat from './sounds/sound-files/hithat.wav';
+import clap from './sounds/sound-files/clap.wav';
+import bass from './sounds/sound-files/bass-0102.wav';
+import useEventListener from "@use-it/event-listener";
+import styled from 'styled-components';
+import Slider from 'react-rangeslider';
+import Switch from "react-switch";
+
+// CSS constants;
+const babyBlue = '#C3E0E5';
+const darkBlue = '#274472';
+const blueGray = '#5885AF';
+const midnightBlue = '#41729F';
+const buttonSize = '100px';
+
+// Styled components;
+const StyledHeader = styled.h1`
+  color: ${darkBlue};
+  width: 100%;
+`
+const DrumPads = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  max-width: 330px;
+`
+const DrumButton = styled.button`
+  width: ${buttonSize};
+  height: ${buttonSize};
+  background: linear-gradient(270deg, ${darkBlue} 0%, ${midnightBlue} 100%);
+  border: none;
+  color: ${babyBlue};
+  margin-top: 10px;
+`
+const SetupContainer = styled.div`
+  min-width: 200px;
+  max-width: 300px;
+  flex-grow: 1;
+  margin: 10px 5px 0 5px;
+  border: 1px solid ${blueGray};
+  background: linear-gradient(0deg, rgba(255,255,255,1) 0%, ${babyBlue} 100%);;
+  display: flex;
+  align-items: stretch;
+  flex-direction: column;
+  justify-content: start;
+  padding: 10px;
+`
+const InfoDisplay = styled.div`
+  background-color: black;
+  font-family: 'Gugi', cursive;
+  margin: 10px;
+  color: white;
+  height: 35px;
+  font-size: 20px;
+  line-height: 20px;
+  padding: 5px;
+  padding-top: 15px;
+`
 
 function App() {
+  // hook for volume;
+  const [volume, setVolume] = useState(0.5);
+  // hooks for sounds;
+  const [playKick] = useSound(drumKick, {volume: volume});
+  const [playSnare] = useSound(snare, {volume: volume});
+  const [playCowbell] = useSound(cowbell, {volume: volume});
+  const [playCowbell2] = useSound(cowbell2, {volume: volume});
+  const [playHithat] = useSound(hithat, {volume: volume});
+  const [playRide] = useSound(ride, {volume: volume});
+  const [playClap] = useSound(clap, {volume: volume});
+  const [playChord] = useSound(chord, {volume: volume});
+  const [playBass] = useSound(bass, {volume: volume});
+  // hook for diplaying last sound;
+  const [display, setDisplay] = useState('');
+  // turning on & off the Drum Machine;
+  const [working, setWorking] = useState(true);
+  // function for turning off funcionalities of Drum Machine; 
+  const handleChange = () => (working ? (setWorking(false), setDisplay('')) : setWorking(true));
+  
+  const sounds = [
+    {name: 'Kick', soundType: playKick}, 
+    {name: 'Snare', soundType: playSnare},
+    {name: 'CowBell', soundType: playCowbell},
+    {name: 'CowBell 2', soundType: playCowbell2},
+    {name: 'Hit Hat', soundType: playHithat},
+    {name: 'Ride', soundType: playRide},
+    {name: 'Clap', soundType: playClap},
+    {name: 'Chord', soundType: playChord},
+    {name: 'Bass', soundType: playBass}
+  ];
+
+  const playKeys = ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'];
+
+  const buttons = sounds.map((value, index) => (
+    <DrumButton
+      key={index}
+      onClick={() => {value.soundType(); setDisplay(value.name)}}
+    >
+      Key: {playKeys[index].toUpperCase()}<br/>{value.name}
+    </DrumButton>
+  ));
+  // the buttons we will render when the machine is turned off;
+  const offButtons = sounds.map((value, index) => (
+    <DrumButton key={index}></DrumButton>
+  ));
+  // event listener for keyboard events
+  useEventListener('keydown', e => {
+    for (let i = 0; i <=9; i++) {
+      if(e.key === playKeys[i]) {
+        sounds[i].soundType();
+        setDisplay(sounds[i].name);  
+      }
+    }
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div className="App">
+        <StyledHeader>Drum Machine App</StyledHeader>
+        <DrumPads>
+          {working ? buttons : offButtons}
+        </DrumPads>
+        <SetupContainer>
+          <InfoDisplay>{display}</InfoDisplay>
+          {working &&
+          <Slider
+            max={1}
+            min={0}
+            step={0.1}
+            tooltip={false}
+            value={volume}
+            onChange={
+              value => {
+                setVolume(value);
+                setDisplay(`Volume: ${value.toFixed(1)}`);
+                }}
+          />
+          }
+          <Switch className='switch'
+            checked={working}
+            onChange={handleChange}
+            onColor={darkBlue}
+          />
+        </SetupContainer>
+      </div>
   );
 }
 
